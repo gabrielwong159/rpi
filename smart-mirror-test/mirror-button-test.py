c64ca8dc8a52300e48ff7c8829bfe7a4e1281b4b
Tzeusy <uniquosity@gmail.com>
2017-01-12 02:06:00 +0800

    Fixed the mirror button test

--- smart-mirror-test/mirror-button-test.py
+++ smart-mirror-test/mirror-button-test.py
@@ -10,7 +10,7 @@ KEYS = [uinput.KEY_1, uinput.KEY_2, uinput.KEY_3, uinput.KEY_4, uinput.KEY_5, ui
 device = uinput.Device(KEYS)
 
 # green, green, yellow, blue, red, pink, black, white
-BUTTONS = [13, 6, 20, 19, 12, 16, 21, 5]
+BUTTONS = [6, 13, 20, 19, 12, 16, 21, 5]
 
 prev = [False, False, False, False, False, False, False, False]
 curr = [False, False, False, False, False, False, False, False]
@@ -25,18 +25,15 @@ def buttonPressed():
             if GPIO.input(BUTTONS[i]):
                 curr[i] = True
                 if prev[i] == False:
-                    return i
+                    device.emit_click(KEYS[i])
             else:
                 curr[i] = False
 
-        return -1
+	    prev[i] = curr[i]
 
 while True:
 	try:
-            button = buttonPressed()
-	    if button >= 0:
-                device.emit_click(KEYS[i])
-
+            buttonPressed()
 	except KeyboardInterrupt:
             print "\nexiting"
             GPIO.cleanup()
